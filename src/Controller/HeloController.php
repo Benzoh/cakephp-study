283e076bede49fb7854911d4bc5ca29aefe3ee3a
Benzoh.Wada <benzoh.wada@gmail.com>
2017-12-27 08:37:53 +0900

    フォームの送信

--- src/Controller/HeloController.php
+++ src/Controller/HeloController.php
@@ -9,7 +9,14 @@ class HeloController extends AppController {
     // http://localhost:8765/helo
     // http://localhost:8765/helo/index
     public function index() {
-        $this->set('message', 'Helo!');
+        $str = $this->request->data('text1');
+        // リクエスト方法のチェック
+        var_dump($this->request->is('post'));
+        if ($str != null) {
+            $this->set('message', 'you typed' . $str);
+        } else {
+            $this->set('message', 'please type...');
+        }
     }
 
     // 最初redirect()と書いたがエラー。予約語かな。
--- src/Template/Helo/index.ctp
+++ src/Template/Helo/index.ctp
@@ -4,4 +4,9 @@
         <li>This is sample.</li>
     </ul>
     <p><?= $message ?></p>
+
+    <form action="/helo/index" method="post">
+        <input type="text" name="text1">
+        <input type="submit">
+    </form>
 </div>
