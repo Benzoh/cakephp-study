30aa8672f045cd22d41040ccac2b0d20b4d9cd21
Benzoh.Wada <benzoh.wada@gmail.com>
2017-12-28 08:11:38 +0900

    クエリーパラメータ

--- src/Controller/HeloController.php
+++ src/Controller/HeloController.php
@@ -9,14 +9,20 @@ class HeloController extends AppController {
     // http://localhost:8765/helo
     // http://localhost:8765/helo/index
     public function index() {
-        $str = $this->request->data('text1');
-        // リクエスト方法のチェック
-        var_dump($this->request->is('post'));
-        if ($str != null) {
-            $this->set('message', 'you typed' . $str);
-        } else {
-            $this->set('message', 'please type...');
-        }
+        // $str = $this->request->data('text1');
+        // // リクエスト方法のチェック
+        // var_dump($this->request->is('post'));
+        // if ($str != null) {
+        //     $this->set('message', 'you typed' . $str);
+        // } else {
+        //     $this->set('message', 'please type...');
+        // }
+
+        $id = $this->request->query('id');
+        $name = $this->request->query('name');
+        $this->set('message', 'your id:' . $id . ', name:' . $name);
+
+        // http://192.168.100.100/helo/index?id=1001&name=john
     }
 
     // 最初redirect()と書いたがエラー。予約語かな。
