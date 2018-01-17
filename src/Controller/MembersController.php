5a91d79e279578d2a1db2d0d46391efc33390513
Benzoh.Wada <benzoh.wada@gmail.com>
2018-01-18 08:21:18 +0900

    アソシエーション利用のデータアクセス（1） Membersと、Messagesの関係 Membersのデータ取得

---
+++ src/Controller/MembersController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\Datasource\ConnectionManager;
+use Cake\Validation\Validator;
+
+class MembersController extends AppController {
+
+    public $paginate = [
+        'limit' => 5,
+        'order' => [
+            'Persons.name' => 'asc'
+        ]
+    ];
+
+    public $helpers = [
+        'Paginator' => ['templates' => 'paginator-templates']
+    ];
+
+    public function initialize() {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+    }
+
+    public function index() {
+        $this->set('members', $this->paginate($this->Members));
+        $this->set('_serialize', ['members']);
+    }
+
+}
---
+++ src/Template/Members/index.ctp
@@ -0,0 +1,18 @@
+<pre>index.ctp</pre>
+<tbody>
+<?php foreach ($members as $member): ?>
+<tr>
+    <td><?= $this->Number->format($member->id) ?></td>
+    <td><?= __($member-name) ?></td>
+    <td class="actions">
+        <?= $this->HMTL->link(__('View'), ['action' => 'view', $member->id]) ?>
+        <?= $this->HTML->link(__('Edit'), ['action' => 'edit', $member->id]) ?>
+        <?= $this->Form->postLink(
+            __('Delete'),
+            ['action' => 'delete', $member->id],
+            ['confirm' => __('Are you sure you want to delete #{0}?', $member->id)]
+        ) ?>
+    </td>
+</tr>
+<?php endforeach; ?>
+</tbody>
