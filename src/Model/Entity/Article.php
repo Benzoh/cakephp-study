a3c81032ed838bcb89b12dbf59f958f3af0a1179
Benzoh.Wada <benzoh.wada@gmail.com>
2018-01-28 12:06:06 +0900

    タグ付け体験の改善  - 計算フィールドの追加  - ビューの更新

--- src/Model/Entity/Article.php
+++ src/Model/Entity/Article.php
@@ -2,6 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Collection\Collection;
 
 /**
  * Article Entity
@@ -44,4 +45,19 @@ class Article extends Entity
         'id' => false,
         'slug' => false,
     ];
+
+    protected function _getTagString()
+    {
+        if (isset($this->_properties['tag_string'])) {
+            return $this->_properties['tag_string'];
+        }
+        if (empty($this->tags)) {
+            return '';
+        }
+        $tags = new Collection($this->tags);
+        $str = $tags->reduce(function ($string, $tag) {
+            return $string . $tag->title . ', ';
+        }, '');
+        return trim($str, ', ');
+    }
 }
