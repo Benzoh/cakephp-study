b5dfea08230a8b3a9b3d6996723c5fc751f9d2bc
Benzoh.Wada <benzoh.wada@gmail.com>
2018-01-31 09:01:20 +0900

    # モデルのないフォーム − フォームの作成

---
+++ src/Form/ContactForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use Cake\Form\Form;
+use Cake\Form\Schema;
+use Cake\Validation\Validation;
+
+class ContactForm extends Form
+{
+    protected function _buildSchema(Schema $schema)
+    {
+        return $schema->addField('name', 'string')
+            ->addField('email', ['type' => 'string'])
+            ->addField('body', ['type' => 'text']);
+    }
+
+    protected function _buildValidator(Validator $validator)
+    {
+        return $validator->add('name', 'length', [
+                'rule' => ['minlength', 10],
+                'message' => '名前は必須です'
+            ])->add('email', 'format', [
+                'rule' => 'email',
+                'message' => '有効なメールアドレスが要求されます',
+            ]);
+    }
+
+    protected function _execute(array $data)
+    {
+        // メール送信する
+        return true;
+    }
+}
